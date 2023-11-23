b3d9d63f0c2420b80c292e52a0697668e36bcf8f
matgon951016 <mateo.gonzalez.escudero@gmail.com>
2023-11-22 20:52:01 -0500

    fix(Connection):Se ajusta ruta BD

--- PetsBizAdvizor/src/Modelo/Conexion.java
+++ PetsBizAdvizor/src/Modelo/Conexion.java
@@ -10,7 +10,7 @@ public class Conexion {
 
     public Connection getConnection() {
         try {
-            String myBD = "jdbc:mysql://localhost:3306/petsbizadvizor?serverTimezone=UTC";
+            String myBD = "jdbc:mysql://localhost:3306/petbizadvisor?serverTimezone=UTC";
             con = DriverManager.getConnection(myBD, "root", "");
             return con;
         } catch (SQLException e) {
